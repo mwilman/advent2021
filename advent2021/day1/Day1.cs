25955ff48ddecee12ba883864a9aad4e19f6d7d5
Maxim Wilman <maxim.wilman.extern@signal-iduna.de>
2021-12-03 09:10:06 +0100

    small refactoring

---
+++ advent2021/day1/Day1.cs
@@ -0,0 +1,69 @@
+﻿namespace advent2021.day1;
+
+public static class Day1
+{
+    public static void DoWork()
+    {
+//Day1
+        Console.WriteLine();
+        Console.WriteLine("Day 1");
+
+        string text = File.ReadAllText(@"day1/day1.txt");
+//Console.WriteLine(text);
+
+        string[] lines = text.Split(Environment.NewLine);
+
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+
+        int count = 0;
+        List<int> list = lines.Select(item => int.Parse(item)).ToList();
+
+        Part1(list, count);
+
+        count = 0;
+
+        Part2(list, count);
+    }
+
+    private static void Part2(List<int> list, int count)
+    {
+        for (int i = 3; i < list.Count; i++)
+        {
+            IEnumerable<int> previousSet = list.Skip(i - 3).Take(3);
+            IEnumerable<int> currentSet = list.Skip(i - 2).Take(3);
+
+            if (previousSet.Sum() < currentSet.Sum())
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Lösung 1 Teil2: {count}");
+    }
+
+    private static void Part1(List<int> list, int count)
+    {
+        for (int i = 1; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i]);
+            if (list[i] > list[i - 1])
+            {
+                count++;
+                Console.WriteLine("Increase");
+            }
+            else
+            {
+                Console.WriteLine("Decrease");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine($"Lösung 1: {count}");
+    }
+}
---
+++ advent2021/day2/Day2.cs
@@ -0,0 +1,90 @@
+﻿namespace advent2021.day2;
+
+public static class Day2
+{
+    public static void DoWork()
+    {
+        string s;
+        string[] strings;
+        // Day 2
+        Console.WriteLine();
+        Console.WriteLine("Day 2");
+
+        s = File.ReadAllText(@"day2/day2.txt");
+        strings = s.Split(Environment.NewLine);
+
+        int[,] position = new int[0, 0];
+        int horizontal = 0;
+        int depth = 0;
+
+        Part1(strings, horizontal, depth);
+        Console.WriteLine();
+        Console.WriteLine();
+
+        int aim = 0;
+        depth = 0;
+        horizontal = 0;
+        Part2(strings, horizontal, depth, aim);
+    }
+
+    private static void Part2(string[] strings, int horizontal, int depth, int aim)
+    {
+        foreach (string line in strings)
+        {
+            string[] opAndNumber = line.Split(' ');
+            string op = opAndNumber[0];
+            int number = int.Parse(opAndNumber[1]);
+
+            switch (op)
+            {
+                case "forward":
+                    Console.WriteLine("forward");
+                    horizontal += number;
+                    depth += aim * number;
+                    break;
+                case "up":
+                    Console.WriteLine("up");
+                    aim -= number;
+                    break;
+                case "down":
+                    Console.WriteLine("down");
+                    aim += number;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        Console.WriteLine($"Endgültige Lösung Day2 Part 2: {horizontal * depth}");
+    }
+
+    private static void Part1(string[] strings, int horizontal, int depth)
+    {
+        foreach (string line in strings)
+        {
+            string[] opAndNumber = line.Split(' ');
+            string op = opAndNumber[0];
+            int number = int.Parse(opAndNumber[1]);
+
+            switch (op)
+            {
+                case "forward":
+                    Console.WriteLine("forward");
+                    horizontal += number;
+                    break;
+                case "up":
+                    Console.WriteLine("up");
+                    depth -= number;
+                    break;
+                case "down":
+                    Console.WriteLine("down");
+                    depth += number;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        Console.WriteLine($"Endgültige Lösung Day2 Part 1: {horizontal * depth}");
+    }
+}
